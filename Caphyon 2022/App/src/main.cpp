9c9ba0caff9816d3341beb06b1d0a18584afca2e
Silvian Achim <silvianachim@hotmail.com>
2022-05-09 17:24:13 +0300

    fx

--- Caphyon 2022/App/src/main.cpp
+++ Caphyon 2022/App/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
   //Before run make sure that project app is an application(.exe)
-  //For testing change project App to static library(.lib)
+  //To run TestCases project change project App to static library(.lib)
   std::string inputFile = "liniaro-planet.in";
   std::string outputFile = "liniaro-planet.out";
   Solve mSolve(inputFile, outputFile);
